c307c76636dfc0c24f0aa22b208175d3289a4d11
Herz, Andreas <andreas.herz@sap.com>
2022-03-17 11:44:37 +0100

    no message

--- src/web/webpack.config.js
+++ src/web/webpack.config.js
@@ -1,48 +1,55 @@
 const path = require('path');
+const webpack = require('webpack');
 
 const projectDir = path.resolve(__dirname)
 
 module.exports = {
-    entry: projectDir + '/js/index.js',
-    devtool: 'source-map',
-    mode: 'development',
-    output: {
+  entry: projectDir + '/js/index.js',
+  devtool: 'source-map',
+  mode: 'development',
+  output: {
     libraryTarget: 'umd', // make the bundle export
-      path: projectDir + '/js/webpack',
-      filename: "bundle.js"
-    },
-    resolve: {
-        modules: [ projectDir+'/node_modules', projectDir+'/src'],
-          extensions: ['.json', '.js', '.css']
-    },    module: {
+    path: projectDir + '/js/webpack',
+    filename: "bundle.js"
+  },
+  resolve: {
+    modules: [projectDir + '/node_modules', projectDir + '/src'],
+    extensions: ['.json', '.js', '.css']
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      CircuitFigure: './figures/CircuitFigure'
+    })
+  ],
+  module: {
     rules: [
-        {
-          test: /\.less$/i,
-          use: [
-            // compiles Less to CSS
-            "style-loader",
-            "css-loader",
-            "less-loader",
-          ],
-        },
-        {
-            test: /\.svg$/,
-            loader: 'svg-inline-loader'
-        },
-        {
-            test: /\.css$/i,
-            use: ["style-loader", "css-loader"],
-        },
-        {
-            test: /\.m?js$/,
-            exclude: /(node_modules|bower_components)/,
-            use: {
-              loader: 'babel-loader',
-              options: {
-                presets: ['@babel/preset-env']
-              }
-            }
+      {
+        test: /\.less$/i,
+        use: [
+          // compiles Less to CSS
+          "style-loader",
+          "css-loader",
+          "less-loader",
+        ],
+      },
+      {
+        test: /\.svg$/,
+        loader: 'svg-inline-loader'
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
         }
-      ],
-    },
-  };
+      }
+    ],
+  },
+};
